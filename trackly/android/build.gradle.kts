1b77b4eb5514cadeed6f3c1557af3b5aa192f31a
iissiin <issina080208@gmail.com>
2026-05-18 01:08:26 +0500

    wip чуть чуть шаман

--- trackly/android/build.gradle.kts
+++ trackly/android/build.gradle.kts
@@ -15,14 +15,11 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-
-plugins {
-  id("com.google.gms.google-services") version "4.3.15" apply false
 }
